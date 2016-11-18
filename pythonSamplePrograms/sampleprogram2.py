63ba623786be7ec35f5a3fe326077505dee5a351
Sneha Alkuru <alkurusneharayal@gmail.com>
2016-11-18 09:56:33 -0600

    demonstrate to give input and print.
    This example demonstartes to play with strings.Also, how to give input and print them.

---
+++ pythonSamplePrograms/sampleprogram2.py
@@ -0,0 +1,16 @@
+>>> 'Hello' + 'World'
+'HelloWorld'
+>>> 'Sneha'+ 'Alkuru'
+'SnehaAlkuru'
+>>> 'Sneha' + ' ' + 'Alkuru'
+'Sneha Alkuru'
+>>> 'Sneha' * 3
+'SnehaSnehaSneha'
+>>> 'Sneha' + '!' *10
+'Sneha!!!!!!!!!!'
+>>> text= input("Enter your name:")
+Enter your name:Sneha
+>>> print('your name is:'+text)
+your name is:sneha
+>>> print ('your age will be ' +str(int(age) + 1)+' next year')
+your age will be 3 next year
