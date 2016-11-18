f3638627e757d0e9fbd27d9035dd600797ce2b83
knklalit <tanakaryuji91@gmail.com>
2016-11-17 23:42:25 -0600

    Add a script that will rename files with blanks in their name
     Also made some changes to the previos colorScript. Will have to read more about sed and awk. Just a vague understanding on sed and awk

--- bashSampleScripts/colorBckgrnd.sh
+++ bashSampleScripts/colorBckgrnd.sh
@@ -2,7 +2,6 @@
 # THis script turns the screen green
 cd /c/Users/S525729/Google Drive/Semester-3/GDP-1/gitsubmit_cli/bashSampleScripts
 #ask Dr.Eloe on how to redirect pwd output to some other function or variable
-cd $path
 pwd
 setterm -background green
 echo Changed color to green
