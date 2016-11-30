cb1adca69a0afb95c6c8cb34009fbdf6fc3d3c6b
knklalit <tanakaryuji91@gmail.com>
2016-11-30 10:03:47 -0600

    Correct the code so as to searches for file with pub extension

--- bashSampleScripts/AuthenticationInBash.sh
+++ bashSampleScripts/AuthenticationInBash.sh
@@ -4,8 +4,8 @@ echo
 echo
 echo "Please wait while we check for existing keys for authentication"
 echo
-read -p 'Provide the path of the directory where you would store your keys, i.e, the location of .ssh folder on your system' target
-name = '*.pub'
+read -p 'Provide the path of the directory where you would store your keys, i.e, the location of .ssh folder on your system: ' target
+#name = '*.pub'
 
 if [ ! "$(ls -A $target)" ]; then
     echo -e "Directory $target is empty"
@@ -16,13 +16,14 @@ found=0
 while read line; do
     found=$[found+1]
     echo -e "Found: $line"
-done < <(find . -name '*.pub' -o -print | grep keys)
+done < <(find . -name '*.pub' -o | grep keys)
 
 if [[ "$found" == "0" ]]; then
-    echo -e "No match for $name"
-    echo "We will now go ahead generate the keys required for authentication"
+    echo -e "No match for files with .pub extension"
+    echo "We will now generate the keys required for authentication"
     # we need to move to home folder to make the.ssh folder
-  # mkdir ~/.ssh
+    cd $target
+    mkdir ~/.ssh
     chmod 700 ~/.ssh
     ssh-keygen -t rsa
 else
