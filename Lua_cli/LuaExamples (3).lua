07f40ab4962991f63d396bfa53068b1a6c9ffa35
sushmagaddam <sushmagsar@gmail.com>
2016-11-30 02:46:58 -0600

    Add simplified code to formatting numbers

---
+++ Lua_cli/LuaExamples (3).lua
@@ -0,0 +1,19 @@
+function comma_value(amount)
+  local formatted = amount
+  while true do  
+    formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+    if (k==0) then
+      break
+    end
+  end
+  return formatted
+end
+-- rounds a number to the nearest decimal places
+--
+function round(val, decimal)
+  if (decimal) then
+    return math.floor( (val * 10^decimal) + 0.5) / (10^decimal)
+  else
+    return math.floor(val+0.5)
+  end
+end
