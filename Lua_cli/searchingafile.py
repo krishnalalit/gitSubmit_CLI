0e2d7919f8cfd42ef48ef3a8e32f029dfd7e7920
sushmagaddam <sushmagsar@gmail.com>
2017-01-23 08:34:12 -0600

    This code will make us understand searching a website for a page
    Demonstrate searching a specific web site for the page

---
+++ Lua_cli/searchingafile.py
@@ -0,0 +1,24 @@
+def dosearch(terms, searchtype, case, adddir, files = []):
+    found = []
+
+    if files != None:
+        titlesrch = re.compile('>title<.*>/title<')1
+        for file in files:
+		title = ""
+            if not (file.lower().endswith("html") or file.lower().endswith("htm")):
+                continue2
+
+            filecontents = open(BASE_DIR + adddir + file, 'r').read()
+
+            titletmp = titlesrch.search(filecontents)3
+            if titletmp != None:
+                title = filecontents.strip()[titletmp.start() + 7:titletmp.end() - 8]
+
+            filecontents = remove_tags(filecontents)4
+            filecontents = filecontents.lstrip()
+            filecontents = filecontents.rstrip()
+
+	if dofind(filecontents, case, searchtype, terms) > 0:5
+                found.append(title)
+                found.append(file)
+	return found6
