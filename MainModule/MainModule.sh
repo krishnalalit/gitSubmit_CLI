54479e1c7618afd9b06efe2f6181cf4d2656d845
knklalit <tanakaryuji91@gmail.com>
2017-02-21 20:06:09 -0600

    Corrected existing code for errors during testing of previous commit
    Also added VM for testing as bash for windows is a bit different than linux and scipts running on windows arent running on linux

--- MainModule/MainModule.sh
+++ MainModule/MainModule.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+echo
+echo
 echo "Welcome to GitSubmit Command Line Interface "
       echo
       echo
 
-echo "Please wait while we check for existing keys for authentication"
+echo "Please wait while we check for existing keys, for authentication"
       echo
       echo "The following keys were found"
       file * | grep RSA
@@ -12,31 +13,43 @@ echo "Please wait while we check for existing keys for authentication"
       echo -e "Total: $found keys"
       echo
 
-uname = "$(git config --global --list | wc -l)"
-      if[["$uname" <=2]]; then
-        read -p "Please set a username for your account" name
-        git config --global user.name "$name"
-        echo "This is the username you have entered: "
-        git config --global user.name
-        echo
-        read -p "Please enter the email address you would like to associate with this account" email
-        echo
-        echo "This is the email you have entered: "
-        git config --global user.email "$email"
-      else
-        echo
-        echo "A username had already been set. The following are the details: "
-        echo
-        git config --global --list
-      fi;
+if [[ "$found" == "0" ]]; then
+    echo 'Enter your GitLab username: '
+    read u
+    echo 'Enter your GitLab password: '
+    read -s p
 
-read -p "Would you like to use the existing keys for authentication (yes or no) ?" response
-if [[ "$response" == "yes" ]]; then
-      echo " Thank you. We will proceed with authentcation using the existing keys"
-      echo
-      # This is where the python script to clone and search would be used
+    echo 'Generating GitLab authentication key ...'
+    ssh-keygen -t rsa -b 4096 -C "$u" -P "" -f '/home/ubuntu/.ssh/ssh_git'
+
+    echo 'Authorising key for use with GitLab ...'
+    CFG='\nHost gitlab.com\n  HostName gitlab.com\n  User git\n  IdentityFile /home/ubuntu/.ssh/ssh_git'
+    echo -e $CFG >> /home/ubuntu/.ssh/config
+    chmod 400 /home/ubuntu/.ssh/ssh_git
+
+    echo 'Finished configuring keys ... '
+
+    cat /home/ubuntu/.ssh/ssh_git.pub
+    echo '^ Copy the key above in your GitLab settings to authorise your username'
+
 else
-      echo 'Generating GitLab authentication key ...'
-      ssh-keygen -t rsa -b 4096 -C "$u" -P "" -f '/home/ubuntu/gitlabKeys/ssh_gitLabKey'
-      # Call python script to copy the ssh key generated to server
+    read -p "Would you like to use the existing keys for authentication (yes or no) ?" response
+    if [[ "$response" == "yes" ]]; then
+        echo " Thank you. We will proceed with authentcation using the existing keys"
+        echo
+        # This is where the python script to clone and search would be used
+    else
+        echo 'Generating GitLab authentication key ...'
+        ssh-keygen -t rsa -b 4096 -C "$u" -P "" -f '/home/ubuntu/.ssh/ssh_git'
+
+        echo 'Authorising key for use with GitLab ...'
+        CFG='\nHost gitlab.com\n  HostName gitlab.com\n  User git\n  IdentityFile /home/ubuntu/.ssh/ssh_git'
+        echo -e $CFG >> /home/ubuntu/.ssh/config
+        chmod 400 /home/ubuntu/.ssh/ssh_git
+
+        echo 'Finished configuring keys ... '
+
+        cat /home/ubuntu/.ssh/ssh_git.pub
+        echo '^ Copy the key above in your GitLab settings to authorise your username'
+        # Call python script to copy the ssh key generated to server
 fi;
