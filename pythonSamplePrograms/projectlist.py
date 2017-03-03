982d7b4842caf09245d7a6754b90ffe222e57c78
Sneha Alkuru <alkurusneharayal@gmail.com>
2017-03-03 08:18:58 -0600

    demostrate implementation of fetching url from git lab to  clone
    This script displays all the projrcts which are in git lab server

---
+++ pythonSamplePrograms/projectlist.py
@@ -0,0 +1,26 @@
++import gitlab
+
++
+
++# gitlab connection using username password
+
++#gl = gitlab.Gitlab('https://gitlab.com', email='sushmagsar@gmail.com', password='Bunny$20945')
+
++
+
++#gitlab connection using token
+
++gl = gitlab.Gitlab('https://gitlab.com', '9yRKey51NiMGmKxxbAtk')
+
++
+
++gl.auth()
+
++
+
++# list all the projects
+
++projects = gl.projects.list()
++for project in projects:
+
++    print(project)
