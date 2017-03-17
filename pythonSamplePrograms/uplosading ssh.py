8557c88a858ac8238a5e241b912d4e2b713ef547
Sneha Alkuru <alkurusneharayal@gmail.com>
2017-03-17 08:27:51 -0500

    change the location of files

--- pythonSamplePrograms/uplosading ssh.py
+++ pythonSamplePrograms/uplosading ssh.py
@@ -1,5 +1,4 @@
 
-
 import gitlab look top
 #creating an gitlab instance from the config file(should have the details host,username etc) 
 gl = gitlab.Gitlab('http://gitlab.com', email=username, password=password)
@@ -14,4 +13,3 @@ current_user = gl.user
 k = gl.user_keys.create({'title': 'my_key',
 'key': open('/home/ubuntu/.ssh/ssh_git').read()},
 user_id=current_user.user_id)
-
