a5a5027d9b30d2bf194083c88b2b4f4041852b25
Kandriga Lalith <S525729@mail.nwmissouri.edu>
2017-03-28 11:27:33 -0500

    Add code to resolve GCLI#002 ticket's error

--- Tests/bashTesting/Testing-MainModule.sh
+++ Tests/bashTesting/Testing-MainModule.sh
@@ -67,7 +67,7 @@ else
 	#find /etc/apt/ -name *.list | xargs cat | grep  ^[[:space:]]*deb # to check if repo exists on the system
         cd "$path"
 	#GIT_SSH_COMMAND="ssh -i ~/.ssh/gitLabCLIKeys -F /dev/null" git clone git@csgrad06.nwmissouri.edu:S525729/Lalit-cli-test.git
-	ssh-agent sh -c "ssh-add ~/.ssh//gitLabCLIKeys; git clone git@csgrad06.nwmissouri.edu:S525729/Lalit-cli-test.git" 
+	ssh-agent sh -c "ssh-add ~/.ssh/gitLabCLIKeys; cd $path | git clone git@csgrad06.nwmissouri.edu:S525729/Lalit-cli-test.git"
 
     else
         echo 'Generating GitLab authentication key ...'
@@ -89,6 +89,6 @@ else
 	read -p "Enter the path, where you would like to clone your repository: " path
 	cd "$path"
 	#GIT_SSH_COMMAND="ssh -i ~/.ssh/gitLabCLIKeys -F /dev/null" git clone git@csgrad06.nwmissouri.edu:S525729/Lalit-cli-test.git
-	ssh-agent sh -c "ssh-add ~/.ssh//gitLabCLIKeys; git clone git@csgrad06.nwmissouri.edu:S525729/Lalit-cli-test.git" 
+	#ssh-agent sh -c "ssh-add ~/.ssh/gitLabCLIKeys; cd $path |  git clone git@csgrad06.nwmissouri.edu:S525729/Lalit-cli-test.git" 
 fi;
 fi;
