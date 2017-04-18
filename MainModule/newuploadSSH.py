54cc6422b8151b0fca86441d1e7af3f412624698
Kandriga Lalith <S525729@mail.nwmissouri.edu>
2017-04-18 11:17:14 -0500

    Change path of keys being housed in py module

--- MainModule/newuploadSSH.py
+++ MainModule/newuploadSSH.py
@@ -10,7 +10,7 @@ gl.auth()
 
 
 k = gl.user_keys.create({'title': 'my_key',
-                         'key': open('/usr/local/share/ubuntu/.ssh/ssh_git.pub').read()},
+                         'key': open('~/Desktop/GitLabRepo/gitLabCLIKeys.pub').read()},
                         user_id=1)
 
 print(k)
